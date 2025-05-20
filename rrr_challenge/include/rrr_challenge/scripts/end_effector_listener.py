ce48db79b8371633a84e0cc30e31137eed0df7d2
Matteo Zucchelli <mzucchelli@ethz.ch>
2025-05-20 21:04:29 +0200

    visualize correctly target pose

--- rrr_challenge/include/rrr_challenge/scripts/end_effector_listener.py
+++ rrr_challenge/include/rrr_challenge/scripts/end_effector_listener.py
@@ -5,7 +5,7 @@ from rclpy.time import Time # Import Time
 from tf2_ros import TransformListener, Buffer
 from geometry_msgs.msg import TransformStamped, PoseStamped
 from sensor_msgs.msg import JointState
-from nav_msgs.msg import Path
+
 
 class EndEffectorListener(Node):
 
@@ -15,9 +15,7 @@ class EndEffectorListener(Node):
         self.tf_listener = TransformListener(self.tf_buffer, self)
         self.timer = self.create_timer(0.1, self.timer_callback)
         self.ee_publisher_ = self.create_publisher(JointState, '/end_effector_states', 10)
-        self.path_publisher_ = self.create_publisher(Path, '/end_effector_path', 10)
-        self.path_msg = Path()
-        self.path_max_poses = 50
+
         self.get_logger().info('End Effector Listener has been started.')
 
     def timer_callback(self):
@@ -38,27 +36,7 @@ class EndEffectorListener(Node):
             ee_state_msg.position = [trans.transform.translation.z, trans.transform.translation.x]
             self.ee_publisher_.publish(ee_state_msg)
 
-            # self.get_logger().info(
-            #     f"Published end-effector position: x={ee_state_msg.position[0]}, "
-            #     f"y={ee_state_msg.position[1]} relative to {trans.header.frame_id} at time {trans.header.stamp.sec}.{trans.header.stamp.nanosec}"
-            # )
-
-            current_pose = PoseStamped()
-            current_pose.header.stamp = trans.header.stamp
-            current_pose.header.frame_id = trans.header.frame_id
-            current_pose.pose.position.x = trans.transform.translation.x
-            current_pose.pose.position.y = trans.transform.translation.y
-            current_pose.pose.position.z = trans.transform.translation.z
-            current_pose.pose.orientation = trans.transform.rotation
-
-            self.path_msg.header.stamp = trans.header.stamp
-            self.path_msg.header.frame_id = trans.header.frame_id
-            self.path_msg.poses.append(current_pose)
-
-            if len(self.path_msg.poses) > self.path_max_poses:
-                self.path_msg.poses.pop(0)
-
-            self.path_publisher_.publish(self.path_msg)
+          
             
         except Exception as e:
             self.get_logger().warn(f"Could not transform or publish: {e}")
--- rrr_challenge/include/rrr_challenge/scripts/master_node.py
+++ rrr_challenge/include/rrr_challenge/scripts/master_node.py
@@ -2,8 +2,10 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import JointState
+from geometry_msgs.msg import TransformStamped, PoseStamped, Quaternion
 import numpy as np
-import time
+from rclpy.time import Time # Import Time
+from nav_msgs.msg import Path
 
 class MasterNode(Node):
     def __init__(self):
@@ -24,8 +26,13 @@ class MasterNode(Node):
         self.timer_period = 0.02  # seconds
         self.timer = self.create_timer(self.timer_period, self.timer_callback)
 
+        self.path_publisher_ = self.create_publisher(Path, '/end_effector_path', 10)
+        self.path_msg = Path()
+        self.path_max_poses = 10
+
     def timer_callback(self):
         self.publish_target()
+        # Request the latest available transform          
 
     def publish_target(self):
         msg = JointState()
@@ -41,8 +48,29 @@ class MasterNode(Node):
         msg.name = self.task_space_names 
         msg.position = target_position.tolist()
         
+
+        current_pose = PoseStamped()
+        current_pose.header.stamp = msg.header.stamp
+        current_pose.header.frame_id = "base_joint_link"
+        current_pose.pose.position.x = target_position[1]
+        current_pose.pose.position.y = 0.0
+        current_pose.pose.position.z = target_position[0]
+        q = Quaternion()
+        q.x = 0.0
+        q.y = 0.0
+        q.z = 0.0
+        q.w = 1.0
+        current_pose.pose.orientation = q
+
+        self.path_msg.header.stamp = msg.header.stamp
+        self.path_msg.header.frame_id = "base_joint_link"
+        self.path_msg.poses.append(current_pose)
+
+        if len(self.path_msg.poses) > self.path_max_poses:
+            self.path_msg.poses.pop(0)
+        
         self.publisher_.publish(msg)
-        #self.get_logger().info(f'Publishing desired end-effector pose: {msg.position}')
+        self.path_publisher_.publish(self.path_msg)
 
 def main(args=None):
     rclpy.init(args=args)
